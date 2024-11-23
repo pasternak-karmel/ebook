2c1db56444fb5002f6a3bb2a45a2be96f28da9bd
Karmel AVENON <karmelavenon@gmail.com>
2024-11-23 15:22:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -295,7 +295,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 
-export default function BookLandingPage() {
+export default function Home() {
   const router = useRouter();
   const [studentsCount] = useState(12546);
   const featuredBooksRef = useRef<HTMLDivElement>(null);
